8fbcec3bd8d4a0f7bc90a35c1142d8d368f679c9
nts931ag <ntson931@gmail.com>
2022-06-21 17:28:39 +0700

    Initial commit

---
+++ example_21/src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"","/","/home"})
+    public String displayHomePage(Model model){
+        model.addAttribute("username", "ThaiDuy");
+        return "home.html";
+    }
+
+
+}
