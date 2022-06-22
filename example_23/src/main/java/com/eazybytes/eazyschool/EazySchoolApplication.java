71ff4b9412ff551c50b4bcb84144ca9f530b6f8b
Nguyen Thai Son <95063137+nts931ag@users.noreply.github.com>
2022-06-22 08:06:38 +0700

    example_24

---
+++ example_23/src/main/java/com/eazybytes/eazyschool/EazySchoolApplication.java
@@ -0,0 +1,14 @@
+package com.eazybytes.eazyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class EazySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazySchoolApplication.class, args);
+	}
+
+}
